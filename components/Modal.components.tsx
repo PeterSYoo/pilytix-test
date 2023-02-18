d90c27a8c059ec143b9a6497106f02d11b95d943
Peter Yoo <trustintofu@gmail.com>
2023-02-17 18:54:29 -0800

    refactor(Modal): Imports type definitions and replaces `any` types, places charts into their own components and adds them.

--- components/Modal.components.tsx
+++ components/Modal.components.tsx
@@ -1,9 +1,8 @@
-import { ResponsiveBar } from '@nivo/bar';
-import { ResponsiveBullet } from '@nivo/bullet';
-import { Line, ResponsiveLine } from '@nivo/line';
-import { Radar, ResponsiveRadar } from '@nivo/radar';
 import Image from 'next/image';
-import { useState } from 'react';
+import { IOpportunities } from '../types/opportunities';
+import { ProbabilityHistory } from './charts/ProbabilityHistory.components';
+import { FactorsIncreasingWin } from './charts/FactorsIncreasingWin.components';
+import { FactorsDescreasingWin } from './charts/FactorsDescreasingWin.components';
 
 export const Modal = ({
   onClose,
@@ -12,7 +11,7 @@ export const Modal = ({
   handlePreviousItem,
 }: {
   onClose: () => void;
-  rowData: any;
+  rowData: IOpportunities | null;
   handleNextItem: () => void;
   handlePreviousItem: () => void;
 }) => {
@@ -36,7 +35,7 @@ export const Modal = ({
                     />
                   </div>
                 </div>
-                <span>{rowData.oppName}</span>
+                <span>{rowData?.oppName}</span>
               </div>
               <button
                 onClick={onClose}
@@ -51,311 +50,16 @@ export const Modal = ({
               </button>
             </div>
             {/*  */}
+            {/* Charts */}
             <div className="flex flex-col w-full overflow-auto scrollbar-hide gap-[30px]">
               {/* Probability History */}
-              {rowData.probabilityHistory && (
-                <div className="px-[31px] pt-[18px] flex flex-col gap-[9px] w-full">
-                  <span className="text-sm font-medium">Probably History</span>
-                  <div className="border rounded-lg flex justify-center items-center h-[350px] w-full">
-                    {/* Line Chart */}
-                    <div className="hidden lg:block w-screen h-full pl-[100px] -ml-[100px]">
-                      <ResponsiveLine
-                        data={[
-                          {
-                            id: 'Pilytix Probability',
-                            data: rowData.probabilityHistory
-                              .slice()
-                              .reverse()
-                              .map((item: any) => ({
-                                x: `${item.daysAgo}`,
-                                y: item.pilytixProb,
-                              })),
-                          },
-                          {
-                            id: 'Rep Probability',
-                            data: rowData.probabilityHistory
-                              .slice()
-                              .reverse()
-                              .map((item: any) => ({
-                                x: `${item.daysAgo}`,
-                                y: item.repProb,
-                              })),
-                          },
-                        ]}
-                        margin={{ top: 50, right: 40, bottom: 65, left: 70 }}
-                        xScale={{ type: 'point' }}
-                        yScale={{
-                          type: 'linear',
-                          min: 0,
-                          max: 1,
-                          stacked: false,
-                          reverse: false,
-                        }}
-                        axisTop={null}
-                        axisRight={null}
-                        enableGridX={false}
-                        enableGridY={true}
-                        colors={['#69A2F9', '#36C2A0']}
-                        lineWidth={2}
-                        enablePoints={true}
-                        pointSize={6}
-                        pointColor={{ theme: 'background' }}
-                        pointBorderWidth={2}
-                        pointBorderColor={{ from: 'serieColor' }}
-                        pointLabelYOffset={-12}
-                        useMesh={true}
-                        legends={[
-                          {
-                            anchor: 'top-right',
-                            direction: 'row',
-                            justify: false,
-                            translateX: -30,
-                            translateY: -30,
-                            itemsSpacing: 40,
-                            itemDirection: 'left-to-right',
-                            itemWidth: 80,
-                            itemHeight: 20,
-                            itemOpacity: 0.75,
-                            symbolSize: 12,
-                            symbolShape: 'circle',
-                            symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                            effects: [
-                              {
-                                on: 'hover',
-                                style: {
-                                  itemBackground: 'rgba(0, 0, 0, .03)',
-                                  itemOpacity: 1,
-                                },
-                              },
-                            ],
-                          },
-                        ]}
-                        axisBottom={{
-                          tickSize: 5,
-                          tickPadding: 5,
-                          tickRotation: 0,
-                          legend: 'Days Ago',
-                          legendOffset: 50,
-                          legendPosition: 'middle',
-                        }}
-                        axisLeft={{
-                          tickSize: 5,
-                          tickPadding: 5,
-                          tickRotation: 0,
-                          legend: 'Probability',
-                          legendOffset: -50,
-                          legendPosition: 'middle',
-                        }}
-                      />
-                    </div>
-                    {/*  */}
-
-                    {/* Bar Chart */}
-                    <div className="block lg:hidden w-screen h-full pl-[100px] -ml-[100px]">
-                      <ResponsiveBar
-                        data={rowData.probabilityHistory.slice().reverse()}
-                        keys={['pilytixProb', 'repProb']}
-                        indexBy="daysAgo"
-                        margin={{ top: 50, right: 40, bottom: 65, left: 70 }}
-                        padding={0.2}
-                        valueScale={{ type: 'linear' }}
-                        indexScale={{ type: 'band', round: true }}
-                        colors={({ id, data }) => {
-                          if (id === 'pilytixProb') {
-                            return '#69A2F9';
-                          } else {
-                            return '#36C2A0';
-                          }
-                        }}
-                        layout="horizontal"
-                        labelSkipWidth={12}
-                        labelSkipHeight={12}
-                        labelTextColor="white"
-                        legends={[
-                          {
-                            dataFrom: 'keys',
-                            anchor: 'top-right',
-                            direction: 'row',
-                            justify: false,
-                            translateX: -30,
-                            translateY: -30,
-                            itemsSpacing: 40,
-                            itemDirection: 'left-to-right',
-                            itemWidth: 80,
-                            itemHeight: 20,
-                            itemOpacity: 0.75,
-                            symbolSize: 12,
-                            symbolShape: 'circle',
-                            symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                            effects: [
-                              {
-                                on: 'hover',
-                                style: {
-                                  itemBackground: 'rgba(0, 0, 0, .03)',
-                                  itemOpacity: 1,
-                                },
-                              },
-                            ],
-                          },
-                        ]}
-                        axisBottom={{
-                          tickSize: 5,
-                          tickPadding: 5,
-                          tickRotation: 90,
-                          legend: 'Probability',
-                          legendOffset: 50,
-                          legendPosition: 'middle',
-                        }}
-                        axisLeft={{
-                          tickSize: 5,
-                          tickPadding: 5,
-                          tickRotation: 0,
-                          legend: 'Days Ago',
-                          legendOffset: -50,
-                          legendPosition: 'middle',
-                        }}
-                        animate={true}
-                      />
-                    </div>
-                    {/*  */}
-                  </div>
-                </div>
-              )}
+              <ProbabilityHistory rowData={rowData} />
               {/*  */}
               {/* PILYTIX Factors Increasing Win */}
-              {rowData.pilytixFactorsIncreasingWin && (
-                <div className="px-[31px] pt-[18px] flex flex-col gap-[9px] w-full h-full ">
-                  <span className="text-sm font-medium">
-                    PILYTIX Factors Increasing Win
-                  </span>
-                  <div className="border rounded-lg flex justify-center items-center h-[350px] w-full">
-                    {/* Radar Chart */}
-                    <div className="hidden lg:block w-screen h-full pl-[100px] -ml-[100px]">
-                      <ResponsiveRadar
-                        data={rowData.pilytixFactorsIncreasingWin.map(
-                          (factor: any) => ({
-                            key: factor.name,
-                            value: factor.weight.value,
-                            color: 'blue',
-                          })
-                        )}
-                        keys={['value']}
-                        margin={{ top: 65, right: 50, bottom: 65, left: 50 }}
-                        indexBy="key"
-                        maxValue="auto"
-                        curve="linearClosed"
-                        borderWidth={2}
-                        borderColor={() => '#69A2F9'}
-                        gridLevels={5}
-                        gridShape="circular"
-                        gridLabelOffset={36}
-                        enableDots={true}
-                        dotSize={10}
-                        dotColor={() => '#69A2F9'}
-                        dotBorderWidth={2}
-                        dotBorderColor={() => '#69A2F9'}
-                        enableDotLabel={true}
-                        dotLabel="value"
-                        dotLabelYOffset={-12}
-                        colors={() => '#69A2F9'}
-                        fillOpacity={0.25}
-                        blendMode="multiply"
-                        animate={true}
-                      />
-                    </div>
-                    {/*  */}
-
-                    {/* Bar Chart */}
-                    <div className="block lg:hidden w-screen h-full pl-[100px] -ml-[100px]">
-                      <ResponsiveBar
-                        data={rowData.pilytixFactorsIncreasingWin.map(
-                          (factor: any) => {
-                            return {
-                              name: factor.name,
-                              value: factor.weight.value,
-                            };
-                          }
-                        )}
-                        keys={['value']}
-                        indexBy="name"
-                        margin={{ top: 50, right: 30, bottom: 130, left: 65 }}
-                        padding={0.3}
-                        valueScale={{ type: 'linear' }}
-                        indexScale={{ type: 'band', round: true }}
-                        colors={() => '#69A4F9'}
-                        axisBottom={{
-                          tickSize: 5,
-                          tickPadding: 5,
-                          tickRotation: 90,
-                          legend: '',
-                          legendPosition: 'middle',
-                          legendOffset: 32,
-                        }}
-                        axisLeft={{
-                          tickSize: 5,
-                          tickPadding: 5,
-                          tickRotation: 0,
-                          legend: 'Weight',
-                          legendPosition: 'middle',
-                          legendOffset: -50,
-                        }}
-                        labelTextColor="white"
-                      />
-                    </div>
-                    {/*  */}
-                  </div>
-                </div>
-              )}
+              <FactorsIncreasingWin rowData={rowData} />
               {/*  */}
-
               {/* PILYTIX Factors Decreasing Win */}
-              {rowData.pilytixFactorsDecreasingWin && (
-                <div className="px-[31px] pt-[18px] flex flex-col gap-[9px] w-full h-full pb-[100px]">
-                  <span className="text-sm font-medium">
-                    PILYTIX Factors Decreasing Win
-                  </span>
-                  <div className="border rounded-lg flex justify-center items-center h-[350px] w-full">
-                    {/* Bar Chart */}
-                    <div className="w-screen h-full pl-[100px] -ml-[100px]">
-                      <ResponsiveBar
-                        data={rowData.pilytixFactorsDecreasingWin.map(
-                          (factor: any) => {
-                            return {
-                              name: factor.name,
-                              value: factor.weight.value,
-                            };
-                          }
-                        )}
-                        keys={['value']}
-                        indexBy="name"
-                        margin={{ top: 50, right: 30, bottom: 130, left: 65 }}
-                        padding={0.3}
-                        valueScale={{ type: 'linear' }}
-                        indexScale={{ type: 'band', round: true }}
-                        colors={() => '#36C2A0'}
-                        axisBottom={{
-                          tickSize: 5,
-                          tickPadding: 5,
-                          tickRotation: 90,
-                          legend: '',
-                          legendPosition: 'middle',
-                          legendOffset: 32,
-                        }}
-                        axisLeft={{
-                          tickSize: 5,
-                          tickPadding: 5,
-                          tickRotation: 0,
-                          legend: 'Weight',
-                          legendPosition: 'middle',
-                          legendOffset: -50,
-                        }}
-                        labelTextColor="white"
-                      />
-                    </div>
-                    {/*  */}
-                  </div>
-                </div>
-              )}
+              <FactorsDescreasingWin rowData={rowData} />
               {/*  */}
             </div>
             {/*  */}
