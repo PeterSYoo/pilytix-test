9f86c92b2e76f3802c6211379ed4d98edeb185cf
Peter Yoo <trustintofu@gmail.com>
2023-02-17 19:37:49 -0800

    format(index): Formats tailwind classes.

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,10 +87,10 @@ const Home = () => {
           onClose={handleCloseModal}
         />
       ) : null}
-      <div className="w-full h-full grid grid-rows-[54px_44px_59px_1fr] xl:grid-rows-[54px_107px_44px_59px_1fr] xl:max-w-[1405px] mx-auto max-w-[1024px] bg-white">
+      <div className="mx-auto grid h-full w-full max-w-[1024px] grid-rows-[54px_44px_59px_1fr] bg-white xl:max-w-[1405px] xl:grid-rows-[54px_107px_44px_59px_1fr]">
         {/* Header */}
         {/* Row 1 */}
-        <div className="flex justify-between items-center border-t-[5px] border-t-[#69A4F9] border-b border-b-[#F2F2F2] px-[15px] xl:px-[34px]">
+        <div className="flex items-center justify-between border-t-[5px] border-b border-t-[#69A4F9] border-b-[#F2F2F2] px-[15px] xl:px-[34px]">
           <div className="flex items-center text-xs font-medium">
             <span>PILYTIX</span>
             <div className="mt-[1px]">
@@ -114,30 +114,30 @@ const Home = () => {
         </div>
         {/*  */}
         {/* Row 2 */}
-        <div className="hidden xl:flex justify-between items-center px-[34px] pt-[18px] gap-[10px]">
-          <div className="border border-[#F2F2F2] h-full w-full text-xs text-[#D7D7D7] font-bold flex justify-center items-center rounded-lg">
+        <div className="hidden items-center justify-between gap-[10px] px-[34px] pt-[18px] xl:flex">
+          <div className="flex h-full w-full items-center justify-center rounded-lg border border-[#F2F2F2] text-xs font-bold text-[#D7D7D7]">
             PLACEHOLDER
           </div>
-          <div className="border border-[#F2F2F2] h-full w-full text-xs text-[#D7D7D7] font-bold flex justify-center items-center rounded-lg">
+          <div className="flex h-full w-full items-center justify-center rounded-lg border border-[#F2F2F2] text-xs font-bold text-[#D7D7D7]">
             PLACEHOLDER
           </div>
-          <div className="border border-[#F2F2F2] h-full w-full text-xs text-[#D7D7D7] font-bold flex justify-center items-center rounded-lg">
+          <div className="flex h-full w-full items-center justify-center rounded-lg border border-[#F2F2F2] text-xs font-bold text-[#D7D7D7]">
             PLACEHOLDER
           </div>
         </div>
         {/*  */}
         {/* Row 3 */}
-        <div className="w-full border-b-[2px] border-b-[#F2F2F2] px-[15px] h-full xl:px-[34px]">
-          <div className="flex items-end h-full border-b-[2px] border-b-[#44DDB0] w-fit mt-[2px]">
-            <span className="text-sm font-bold pb-[6px]">
+        <div className="h-full w-full border-b-[2px] border-b-[#F2F2F2] px-[15px] xl:px-[34px]">
+          <div className="mt-[2px] flex h-full w-fit items-end border-b-[2px] border-b-[#44DDB0]">
+            <span className="pb-[6px] text-sm font-bold">
               OPPORTUNITIES ({dataOpportunities.length})
             </span>
           </div>
         </div>
         {/*  */}
         {/* Row 4 */}
-        <div className="w-full px-[15px] flex justify-between items-center xl:px-[34px]">
-          <div className="flex items-center gap-[11px] h-[30px] border border-[#DFE1E4] rounded-lg px-[10px]">
+        <div className="flex w-full items-center justify-between px-[15px] xl:px-[34px]">
+          <div className="flex h-[30px] items-center gap-[11px] rounded-lg border border-[#DFE1E4] px-[10px]">
             <Image
               src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1676441190/PILYTIX%20Test/mobile/search_zmpj2l.png"
               alt="search"
@@ -146,20 +146,20 @@ const Home = () => {
             />
             <input
               type="text"
-              className="w-full focus:outline-none text-xs placeholder:text-xs"
+              className="w-full text-xs placeholder:text-xs focus:outline-none"
               placeholder="Search Opportunities"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
           </div>
-          <button className="flex items-center h-[30px] rounded-lg px-[10px] gap-[4px] shadow-[0_3px_0px_0px_rgba(105,162,249,1)] hover:mt-[7px] hover:shadow-none bg-gradient-to-b from-[#69A2F9] to-[#72C6FB]">
+          <button className="flex h-[30px] items-center gap-[4px] rounded-lg bg-gradient-to-b from-[#69A2F9] to-[#72C6FB] px-[10px] shadow-[0_3px_0px_0px_rgba(105,162,249,1)] hover:mt-[7px] hover:shadow-none">
             <Image
               src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1676528284/PILYTIX%20Test/mobile/plus_wavqzz.png"
               alt="add"
               width={12}
               height={12}
             />
-            <span className="text-xs text-white font-medium">
+            <span className="text-xs font-medium text-white">
               New Opportunity
             </span>
           </button>
